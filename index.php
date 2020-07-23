1228c3be17826b34d58dd3e59ca1df592117c2ac
Arno Vanden Driessche <arno@ifolks.local>
2020-07-23 16:28:15 +0200

    prachtige css :)

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Namen</title>
+  <link rel="stylesheet" href="css/main.css">
+
+  <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+
+  <script src="js/main.js"> </script>
+
+
 </head>
 <body>
 
@@ -18,17 +27,42 @@ try {
     $pdo = new PDO("mysql:host=" . servername . ";dbname=" . dbname, user, password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
+ ?>
+
+
+ <?php 
 
     // $sql = $pdo->prepare("SELECT * FROM voornamen ORDER BY naam_id ASC");
     $sql = $pdo->prepare("SELECT * FROM voornamen WHERE geslacht = 'm' ORDER BY totaal_voorkomen DESC LIMIT 100");
     $sql->execute();
+
+    ?>
+
+    <div class="mannen_namen">
+
+    <?php
     while ($result = $sql->fetch(PDO::FETCH_ASSOC)) {
-        echo $result['voornaam']."<br/>" . "Totaal voorkomen: " . $result['totaal_voorkomen'] . "<br>";
+        echo $result['voornaam']."<br/>" . "Totaal voorkomen: " . $result['totaal_voorkomen'] . "<br>" . '<hr>' ;
+    }
+
+    ?>
+    </div>
+
+
+    <div class="vrouwen_namen">
+
+    <?php
+    $sql = $pdo->prepare("SELECT * FROM voornamen WHERE geslacht = 'v' ORDER BY totaal_voorkomen DESC LIMIT 100");
+    $sql->execute();
+    while ($result = $sql->fetch(PDO::FETCH_ASSOC)) {
+        echo $result['voornaam']."<br/>" . "Totaal voorkomen: " . $result['totaal_voorkomen'] . "<br>" . '<hr>';
     }
 
 
+  ?>
 
+    </div>
+   <?php
 
 
   } catch(PDOException $e) {
@@ -37,7 +71,6 @@ try {
   };
  ?>
   
-
 
 </body>
 </html>
